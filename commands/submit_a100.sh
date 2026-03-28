eefc25a08e8a3eb44ea766f8c60ddd8b1eb7a64a
Julius <julius.j.thomas@gmail.com>
2026-03-28 15:20:56 +0100

    move folders

--- commands/submit_a100.sh
+++ commands/submit_a100.sh
@@ -5,8 +5,8 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 #SBATCH --time=12:00:00
-#SBATCH --output=log/slurm/out/slurm-%j.out
-#SBATCH --error=log/slurm/err/slurm-%j.err
+#SBATCH --output=slurm/out/slurm-%j.out
+#SBATCH --error=slurm/err/slurm-%j.err
 
 # Usage: sbatch commands/submit_a100.sh commands/chexpert/explore/runs/iid.sh
 
