cf07c01a1c676460d5551160af7c2968ebd26754
Julius <julius.j.thomas@gmail.com>
2026-03-28 18:39:31 +0100

    update the scripts

--- commands/chexpert/explore/diri.sh
+++ commands/chexpert/explore/diri.sh
@@ -5,7 +5,7 @@
 python3 main.py \
     --exp_name "FedAvg_CheXpert_ResNet18_Diri05" --seed 42 --device cuda \
     --dataset CheXpert --data_path ./external-data \
-    --split_type diri --test_size 0.2 --rawsmpl 0.1 --cncntrtn 0.5 --mincls 2 \
+    --split_type diri --test_size 0.2 --rawsmpl 0.25 --cncntrtn 0.5 --mincls 2 \
     --model_name ResNet18 --resize 224 --hidden_size 32 --imnorm --init_type kaiming \
     --algorithm fedavg --eval_fraction 1 --eval_type both --eval_every 1 --eval_metrics mlacc mlauroc \
     --K 50 --R 100 --E 3 --C 0.2 --B 16 --beta1 0 \
