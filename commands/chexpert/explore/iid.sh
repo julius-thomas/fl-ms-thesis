674ddfd35cbac45cb08a3a5a20df495fe337c311
Julius <julius.j.thomas@gmail.com>
2026-04-01 09:59:45 +0200

    increase model size

--- commands/chexpert/explore/iid.sh
+++ commands/chexpert/explore/iid.sh
@@ -5,8 +5,9 @@
 python3 main.py \
     --exp_name "FedAvg_CheXpert_ResNet18_IID" --seed 42 --device cuda \
     --dataset CheXpert --data_path ./external-data \
-    --split_type iid --test_size 0.2 --rawsmpl 0.25 \
-    --model_name ResNet18 --resize 224 --hidden_size 32 --imnorm --init_type kaiming \
+    --split_type iid --test_size 0.2 --rawsmpl 1.0 \
+    --randhf 0.5 --randrot 10 \
+    --model_name ResNet18 --resize 320 --hidden_size 128 --imnorm --init_type kaiming \
     --algorithm fedavg --eval_fraction 1 --eval_type both --eval_every 1 --eval_metrics mlacc mlauroc \
-    --K 50 --R 100 --E 3 --C 0.2 --B 16 --beta1 0 \
-    --optimizer Adam --lr 0.0001 --lr_decay 0.99 --lr_decay_step 10 --criterion BCEWithLogitsLoss
+    --K 10 --R 100 --E 2 --C 0.5 --B 16 --beta1 0 \
+    --optimizer Adam --lr 0.0001 --lr_decay 1. --lr_decay_step 10 --criterion BCEWithLogitsLoss
