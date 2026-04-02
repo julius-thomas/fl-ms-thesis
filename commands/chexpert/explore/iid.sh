f83b16f90c10b3f223311187ba8bd57e04c55328
nsw510 <nsw510@hendrixgate01fl.unicph.domain>
2026-04-02 11:34:03 +0200

    fixed memory issues

--- commands/chexpert/explore/iid.sh
+++ commands/chexpert/explore/iid.sh
@@ -5,7 +5,7 @@ python3 main.py \
     --dataset CheXpert --data_path ./external-data \
     --split_type iid --test_size 0.2 --rawsmpl 1.0 \
     --randhf 0.5 --randrot 10 \
-    --model_name ResNet18 --resize 320 --hidden_size 128 --imnorm --init_type kaiming \
+    --model_name ResNet18 --resize 224 --hidden_size 64 --imnorm --init_type kaiming \
     --algorithm fedavg --eval_fraction 1 --eval_type both --eval_every 1 --eval_metrics mlacc mlauroc \
     --K 10 --R 100 --E 2 --C 0.5 --B 16 --beta1 0 \
     --optimizer Adam --lr 0.0001 --lr_decay 1. --lr_decay_step 10 --criterion BCEWithLogitsLoss
