a6de94722ee62615c705cbac88aed67d6a2e26de
Julius <julius.j.thomas@gmail.com>
2026-04-13 21:51:35 +0200

    Add submit script for any gpu

---
+++ commands/submit_gpu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=fl
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+#SBATCH --time=18:00:00
+#SBATCH --output=slurm/out/slurm-%j.out
+#SBATCH --error=slurm/err/slurm-%j.err
+
+# Usage: sbatch commands/submit_l40s.sh commands/chexpert/explore/runs/iid.sh
+
+module load cuda/12.8
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate fl
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+source "$1"
