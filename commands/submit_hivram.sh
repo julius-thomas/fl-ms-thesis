b6c48262969359460d692ed096a28c7051e6f53d
Julius <julius.j.thomas@gmail.com>
2026-04-13 21:59:53 +0200

    More submit scripts

--- commands/submit_hivram.sh
+++ commands/submit_hivram.sh
@@ -2,13 +2,14 @@
 #SBATCH --job-name=fl
 #SBATCH -p gpu
 #SBATCH --gres=gpu:1
+#SBATCH --nodelist=hendrixfut01fl,hendrixgpu07fl,hendrixfut03fl,hendrixgpu02fl,hendrixgpu08fl,hendrixgpu15fl,hendrixgpu04fl,hendrixgpu14fl,hendrixgpu21fl,hendrixgpu01fl,hendrixgpu23fl,hendrixgpu24fl,hendrixgpu25fl,hendrixgpu26fl,hendrixgpu16fl
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 #SBATCH --time=18:00:00
 #SBATCH --output=slurm/out/slurm-%j.out
 #SBATCH --error=slurm/err/slurm-%j.err
 
-# Usage: sbatch commands/submit_l40s.sh commands/chexpert/explore/runs/iid.sh
+# Usage: sbatch commands/submit_a100.sh commands/chexpert/explore/runs/iid.sh
 
 module load cuda/12.8
 source ~/miniconda3/etc/profile.d/conda.sh
