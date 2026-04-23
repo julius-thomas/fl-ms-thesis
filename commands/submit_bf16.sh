6d03d5300849430fa0e2c90dae784d648876850a
nsw510 <nsw510@hendrixgpu03fl.unicph.domain>
2026-04-23 14:13:58 +0200

    exclude buggy gpu

--- commands/submit_bf16.sh
+++ commands/submit_bf16.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=fl
 #SBATCH -p gpu
 #SBATCH --gres=gpu:1
-#SBATCH --exclude=hendrixgpu05fl,hendrixgpu06fl,hendrixgpu09fl,hendrixgpu10fl,hendrixgpu11fl,hendrixgpu12fl,hendrixgpu13fl,hendrixgpu17fl,hendrixgpu18fl,hendrixgpu19fl,hendrixgpu20fl
+#SBATCH --exclude=hendrixgpu03fl,hendrixgpu05fl,hendrixgpu06fl,hendrixgpu09fl,hendrixgpu10fl,hendrixgpu11fl,hendrixgpu12fl,hendrixgpu13fl,hendrixgpu17fl,hendrixgpu18fl,hendrixgpu19fl,hendrixgpu20fl
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 #SBATCH --time=18:00:00
